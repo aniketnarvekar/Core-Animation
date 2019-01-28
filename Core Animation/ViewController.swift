1ba0d0c895a5fe9ad1c34d8987bfb5f1a257b0c5
aniketnarvekar <aniket.narvekar0496@gmail.com>
2019-01-28 10:44:42 +0530

    Animating Opacity Animation on UIView of red color rectangle

--- Core Animation/ViewController.swift
+++ Core Animation/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Core Animation
 //
-//  Created by Ethel Azad on 28/01/19.
+//  Created by Aniket Narvekar on 28/01/19.
 //  Copyright © 2019 THK. All rights reserved.
 //
 
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var animatableView: UIView = {
+        let view = UIView(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+        view.backgroundColor = .red
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.animatableView)
+        let from = 0.0
+        let to = 1.0
+        let duration = 3.0
+        let opacityAnimation = changeOpacity(from: from, to: to, with: duration)
+        self.animatableView.layer.add(opacityAnimation, forKey: "opacity")
+        self.animatableView.layer.opacity = 1.0
+        
+    }
+    
+    fileprivate func changeOpacity(from: Any?, to: Any?,with duration: CFTimeInterval ) -> CAAnimation {
+        let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+        opacityAnimation.fromValue = from
+        opacityAnimation.toValue = to
+        opacityAnimation.duration = duration
+        return opacityAnimation
     }
 
 
 }
-
