663005e5e9c9236a5689952b03f2f16a6a1c3f3d
aniketnarvekar <aniket.narvekar0496@gmail.com>
2019-01-28 10:07:55 +0530

    New Blank Project named as "Core Animation"

---
+++ Core Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Animation
+//
+//  Created by Ethel Azad on 28/01/19.
+//  Copyright © 2019 THK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
